dbd5701277232d684a6deb257cd87065d2011f96
ThanhTan00 <tan.lenguyen.cit19@eiu.edu.vn>
2024-05-04 01:24:06 +0700

    install-fe-libraries-and-env-setting

--- movie-fe/src/App.js
+++ movie-fe/src/App.js
@@ -1,9 +1,27 @@
-
+import api from  './api/axiosConfig';
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+      try {
+          const response = await api.get("/api/v1/movies");
+          console.log(response.data);
+          setMovies(response.data);
+      } catch (err){
+          console.log(err);
+      }
+  }
+
+  useEffect(() => {
+      getMovies();
+  },[])
+
   return (
     <div className="App">
-     <p>Le Nguyen Thanh Tan</p>
+
     </div>
   );
 }
